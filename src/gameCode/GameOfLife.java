ccd3e773b7b5aa4c87182ecb9ae6c067d68c2d0b
Vineet <tiwarivineet250@gmail.com>
2025-10-30 22:52:53 +0530

    Added method for counting alive neighbours of cell.

--- src/gameCode/GameOfLife.java
+++ src/gameCode/GameOfLife.java
@@ -1,5 +1,17 @@
 package gameCode;
 
 public class GameOfLife {
-
+	public int countNeighbours(boolean[][] currentBoard,int row,int col) {
+		int rows=currentBoard.length,cols=currentBoard[0].length;
+		int aliveNeighbours=0;
+		for(int i=row-1;i<=row+1;i++) {
+			for(int j=col-1;j<=col+1;j++) {
+				if((i==row&&j==col)||i<0||j<0||i>=rows||j>=cols)
+					continue;
+				if(currentBoard[i][j]==true)
+					aliveNeighbours++;
+			}
+		}
+		return aliveNeighbours;
+	}
 }
